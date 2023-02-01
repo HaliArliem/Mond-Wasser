7e7ca8f9446997f099456b077d76b6fae95310e0
Sam Smith <pytho12tess5opmail@gmail.com>
2023-02-01 14:54:33 +0100

    Deployment

--- build/precache-manifest.80d0fb55c11051e42ba10158dd7831cb.js
+++ build/precache-manifest.80d0fb55c11051e42ba10158dd7831cb.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "5e3ca61dd04e79a5a1c6",
-    "url": "/Mond-Wasser/static/css/main.7c52a7da.chunk.css"
+    "revision": "35a1918b3f415ff261eb",
+    "url": "/Mond-Wasser/static/css/main.4272cadd.chunk.css"
   },
   {
-    "revision": "5e3ca61dd04e79a5a1c6",
-    "url": "/Mond-Wasser/static/js/main.5e3ca61d.chunk.js"
+    "revision": "35a1918b3f415ff261eb",
+    "url": "/Mond-Wasser/static/js/main.35a1918b.chunk.js"
   },
   {
-    "revision": "7d8e8a405abfaeb121e4",
-    "url": "/Mond-Wasser/static/js/1.7d8e8a40.chunk.js"
+    "revision": "24e0880126ecda6556ff",
+    "url": "/Mond-Wasser/static/js/1.24e08801.chunk.js"
   },
   {
     "revision": "00d8a5f1d4ced0b6afcd",
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/Mond-Wasser/static/media/candle.01b57976.gif"
   },
   {
-    "revision": "01c8ba475eafdfa4295b20e7b76cf4aa",
+    "revision": "6a04bd101bdd98e2891f99dc3ed8a80f",
     "url": "/Mond-Wasser/index.html"
   }
 ];
